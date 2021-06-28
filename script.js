738c9e9b2bd9a4266ab534a95f5a4c5d5ad70de9
sakshi262002 <71494682+sakshi262002@users.noreply.github.com>
2021-06-28 12:15:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function compute()
-{
+  {
     p = document.getElementById("principal").value;
     r=document.getElementById("rate").value;
     n=document.getElementById("years").value;
     
-    result=parseInt((p*n*r)/100);
-    document.getElementById('num').innerHTML="SIMPLE INTEREST:"+result;
-    
-}
+    var result=((p*n*r)/100);
+    document.getElementById("result").innerHTML=result;
+
+   }
         
