178bcb5455133373f0a3a4119ad6aa402d01b0de
sakshi262002 <71494682+sakshi262002@users.noreply.github.com>
2021-06-28 10:24:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    r=document.getElementById("rate").value;
+    n=document.getElementById("years").value;
     
 }
         
