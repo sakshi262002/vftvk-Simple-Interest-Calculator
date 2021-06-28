46a264e91c6386a5666c0b64bac1da8c5e39a690
sakshi262002 <71494682+sakshi262002@users.noreply.github.com>
2021-06-28 15:26:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 function compute()
-  {
+	{
+    var p,n,r,a;
     p = document.getElementById("principal").value;
-    r=document.getElementById("rate").value;
-    n=document.getElementById("years").value;
-    
-    var result=((p*n*r)/100);
-    document.getElementById("result").innerHTML=result;
-
-   }
+    n = document.getElementById("years").value;
+    r = document.getElementById("rate").value;
+    a = (p*n*r)/100;
+    var b = Number(a)+Number(p) ; 
+    var s=Number(n)+2021;
+	
+        document.getElementById("result").innerHTML = a;
+	      document.getElementById("amount").innerHTML = b;
+        document.getElementById("pri").innerHTML = p;
+        document.getElementById("ir").innerHTML = r;
+	      document.getElementById("time").innerHTML = s;
+       
+	}
         
