7675f89a16953a4099fd34db20ecfebbf7f0bbdb
sakshi262002 <71494682+sakshi262002@users.noreply.github.com>
2021-06-29 12:24:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 function compute()
 	{
-    var p,n,r,a;
-    p = document.getElementById("principal").value;
-    n = document.getElementById("years").value;
-    r = document.getElementById("rate").value;
-    a = (p*n*r)/100;
-    var b = Number(a)+Number(p) ; 
-    var s=Number(n)+2021;
+    		var n=document.querySelector("select").value;
+    		var p,r,a;
+    		p = document.getElementById("principal").value;
+		if(p<=0)
+		{
+		 alert("Invalid Input!! Enter a Positive number");
+		 return false;
+		}
+		
+    		r = document.getElementById("rate").value;
+   		a = (p*n*r)/100;
+    		var b = Number(p)+ Number(a);
+		var s = Number(n)+2021;
 	
-        document.getElementById("result").innerHTML = a;
-	      document.getElementById("amount").innerHTML = b;
-        document.getElementById("pri").innerHTML = p;
-        document.getElementById("ir").innerHTML = r;
-	      document.getElementById("time").innerHTML = s;
-       
+    		document.getElementById("result").innerHTML = a;
+		document.getElementById("amount").innerHTML = b;
+   		document.getElementById("pri").innerHTML = p;
+		document.getElementById("time").innerHTML = s;
+		document.getElementById("ra").innerHTML = r;
 	}
-        
